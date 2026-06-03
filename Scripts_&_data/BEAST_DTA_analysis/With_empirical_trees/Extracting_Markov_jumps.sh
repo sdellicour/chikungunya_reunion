564a42a037fc6750301ffed2d3a0ef0f3ba610fb
Simon Dellicour <simon.dellicour@ulb.be>
2026-06-03 12:33:39 +0200

    updating repo

---
+++ Scripts_&_data/BEAST_DTA_analysis/With_empirical_trees/Extracting_Markov_jumps.sh
@@ -0,0 +1,3 @@
+perl Extracting_Markov_jumps.pl 25100000 < Alignment_101025_emp3.log > Alignment_101025_emp3.txt
+
+perl Extracting_Markov_jumps.pl 0 < Alignment_101025_100_3.log > Alignment_101025_100_3.txt
